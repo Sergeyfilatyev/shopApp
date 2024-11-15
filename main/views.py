7316f2d5f37de58e092b9138ccdcb478c020bafc
Sergey Filatyev <filatevsergey2407@gmail.com>
2024-11-15 20:29:53 +0200

    main start

--- main/views.py
+++ main/views.py
@@ -4,14 +4,16 @@ from django.shortcuts import render
 
 def index(req):
     context = {
-        "title": "Home",
-        "content": "Main page - Home",
-        "list": [1, 2, 3],
-        "tyr": {"first": "tert"},
-        "bool": False,
+        "title": "Home - Главная",
+        "content": "Магазин мебели - Home",
     }
     return render(req, "main/index.html", context)
 
 
 def about(req):
-    return HttpResponse("About page")
+    context = {
+        "title": "Home - О нас",
+        "content": "О нас",
+        "text_on_page": "Текст о нас , мы классные",
+    }
+    return render(req, "main/about.html", context)
