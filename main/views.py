013b5aeea009a6d777fa0c814eff83981c88d2da
Sergey Filatyev <filatevsergey2407@gmail.com>
2024-11-16 19:05:40 +0200

    fixtures

--- main/views.py
+++ main/views.py
@@ -1,11 +1,17 @@
 
 from django.shortcuts import render
 
+from goods.models import Categories
+
 
 def index(req):
+
+    categories = Categories.objects.all()
+
     context = {
         "title": "Home - Главная",
         "content": "Магазин мебели - Home",
+        "categories": categories
     }
     return render(req, "main/index.html", context)
 
