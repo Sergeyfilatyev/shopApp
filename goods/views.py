e911dade6a5e871058124c639045798b02a60c0b
Sergey Filatyev <filatevsergey2407@gmail.com>
2024-11-22 12:58:32 +0200

    commit

--- goods/views.py
+++ goods/views.py
@@ -4,14 +4,20 @@ from django.shortcuts import get_list_or_404, render
 from goods.models import Products
 
 
-def catalog(req, category_slug):
+def catalog(req, category_slug, page=1):
     if category_slug == "all":
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(3)
-    context = {"title": "Home - Каталог", "goods": current_page}
+    current_page = paginator.page(page)
+
+    context = {
+        "title": "Home - Каталог",
+        "goods": current_page,
+        "slug_url": category_slug,
+    }
     return render(req, "goods/catalog.html", context)
 
 
